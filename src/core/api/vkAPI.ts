539815e1da79958898d8410476d4ad0dd78d592e
Not0range <vanorr2000@gmail.com>
2022-08-29 12:07:39 +0300

    Add types to API

--- src/core/api/vkAPI.ts
+++ src/core/api/vkAPI.ts
@@ -1,14 +1,41 @@
 import { Profile } from "../../types/Profile";
 import { appSettingsProvider } from "../settings";
 
-export const vkLoginUrl = `https://oauth.vk.com/authorize?client_id=${appSettingsProvider.settings.vkClientId}&redirect_uri=https://oauth.vk.com/blank.html&response_type=token`;
+export const vkLoginUrl = `https://oauth.vk.com/authorize?client_id=${appSettingsProvider.settings.vkClientId}&` +
+`redirect_uri=https://oauth.vk.com/blank.html&response_type=token`;
 
 const vkBaseUrl = "https://api.vk.com/method";
 
 const version = "5.131";
 
 export async function GetVkProfile(token: string): Promise<Profile> {
-  const info = (await (await (await fetch(`${vkBaseUrl}/users.get?access_token=${token}&v=${version}`)).json())).response[0];
-  const photos = (await (await fetch(`${vkBaseUrl}/photos.get?owner_id=${info.id}&album_id=profile&count=1&rev=1&access_token=${token}&v=${version}`)).json()).response.items[0].sizes;
+  const info_response = await fetch(`${vkBaseUrl}/users.get?access_token=${token}&v=${version}`);
+  const info = (await info_response.json() as IResponse<IUser>).response[0];
+
+  const photos_response = await fetch(`${vkBaseUrl}/photos.get?owner_id=${info.id}&album_id=profile&count=1&rev=1&access_token=${token}&v=${version}`);
+  const photos = (await photos_response.json() as IResponseItems<{sizes: IPicture[]}>).response.items[0].sizes;
   return { name: `${info.last_name} ${info.first_name}`, photo_url: photos[photos.length - 1].url };
+}
+
+interface IResponse<T> {
+  response: T[]
+}
+
+interface IResponseItems<T> {
+  response: {
+    items: T[]
+  }
+}
+
+interface IUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+}
+
+interface IPicture {
+  url: string;
+  width: number;
+  height: number;
+  type: string;
 }
